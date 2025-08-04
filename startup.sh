f5aaf533ea7b1282c5deb30de3895fbdd0881403
digiopsn8n <digiops.n8n@gmail.com>
2025-08-04 20:34:24 +0200

    Delete startup.sh

--- startup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo "=== INICIANDO APP COMPLETA ==="
-echo "Instalando dependencias..."
-pip install -r requirements_completo.txt
-echo "Iniciando aplicación..."
-python -m uvicorn app_completa:app --host 0.0.0.0 --port 80
